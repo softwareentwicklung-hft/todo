85effd8f05448cbef0fa4fb7c56c795d47d3a096
isabelge <isabel.geissmann@bbz-sh.ch>
2025-08-07 16:34:47 +0200

    hinzufügen von erklärungen

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# todo
+# todo Applikation
+
+Das Ziel ist es eine todo Applikation zu schreiben. In diesem Repository sind die Grundlagen dafür vorgegeben.
+Am Schluss soll es möglich sein, Aufgaben hinzuzufügen, upzudaten und zu löschen. 
+Es müssen zwei Möglichkeiten zur verfügung gestellt werden. Einerseitst muss die Applikation mit einem UI bediehnt werden können 
+und andererseits muss die REST API auch mit CURL bedient werden können.
+
+Momentan werden die Aufgaben gelöscht sobald das Programm gestopt wird. Ziel ist es, dass die Aufgaben dpersisten sind und bei einem neustart das Programms nicht gelöscht werden.
--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ int main() {
     crow::SimpleApp app;
 
     // hier wird die index.html Datei aufgerufen damit das UI angezeigt werden kann
-    // wenn die Datei nicht gefunden wird, wird eine Fehlermeldung ausgegeben
     CROW_ROUTE(app, "/")([]() {
         std::ifstream file("static/index.html");
+        // Datei kann nicht gefunden werden
         if (!file.is_open()) {
             return crow::response(404, "File not found");
         }
@@ -37,27 +37,36 @@ int main() {
     CROW_ROUTE(app, "/todos").methods("GET"_method)([]() {
         std::lock_guard<std::mutex> lock(todos_mutex);
         json j = json::array();
-        for (const auto& todo : todos) {
-            j.push_back({{"id", todo.id}, {"task", todo.task}, {"done", todo.done}});
+        // Schleife um durch alle Aufgabe durchzugehen und diese im json zurückzugeben
+        for (int i = 0; i < todos.size(); ++i) {
+            j.push_back({{"id", todos.at(i).id}, {"task", todos.at(i).task}, {"done", todos.at(i).done}});
         }
+        // zurückgeben von allen Aufgaben
         return crow::response(j.dump());
     });
 
     // füge eine Aufgabe hinzu
     CROW_ROUTE(app, "/todos").methods("POST"_method)([](const crow::request& req) {
-        auto body = json::parse(req.body);
+        // request wird in ein json umgewandelt
+        nlohmann::basic_json<> body = json::parse(req.body);
+        // damit eine neue Aufgabe hinzugefügt werden kann muss diese im Body sein, wenn dieser leer ist wird eine Fehlermeldung ausgegeben
         if (!body.contains("task") || !body["task"].is_string()) {
             return crow::response(400, "Invalid JSON: missing 'task'");
         }
         std::lock_guard<std::mutex> lock(todos_mutex);
+        // Id wird automatisch erstellt, indem wir die Variable next_id für jede Id um eins erhöhen
         Todo newTodo{next_id++, body["task"], false};
+        // mit push_back wird die Aufgabe der "Vektor"-Liste hinzugefügt
         todos.push_back(newTodo);
+        // wir geben zurück, dass die Aufgabe erfolgreich hinzugefügt wurde
         return crow::response(201, json{{"id", newTodo.id}}.dump());
     });
 
     // hier müssen noch zusätzlich DELETE, UPDATE und PUT hinzugefügt werden
     // wie können die einzelnen Aufgaben erkannt werden und wie kann sichergestellt werden, dass die korrekte Aufgabe geupdated wird
     // die Daten werden gelöscht, sobald das Programm geschlossen wird, hier soll noch das Speichern und Einlesen der Daten hinzufügen
+    // Wenn zwei Aufgaben denselben Namen hat, spielt es dann eine Rolle welche von den Aufgaben gelöscht/geupdated wird?
+    //
 
     app.port(18080).multithreaded().run();
 }
